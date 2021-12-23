755a5665775f683433ad0f9e4edcf26626b74aee
CaseyStewart1 <caseystewartdesigns@mail.com>
2021-12-23 12:06:49 -0500

    about page refactoring

--- localeyes/frontEnd/src/components/Find.js
+++ localeyes/frontEnd/src/components/Find.js
@@ -4,9 +4,9 @@ function Find() {
   return (
     <div className="findPage">
       <h1>Where you can find us...</h1>
-      <h2>dummy address</h2>
-      <h3>Phone</h3>
-      <h3>email</h3>
+      <h2>anywhere on those streets of cobblestone</h2>
+      <h3>You cant call us</h3>
+      <h3>You can't email us</h3>
     </div>
   );
 }
