c7b9d0d9d9896a16013a00d7ce71cae9f960481a
CaseyStewart1 <caseystewartdesigns@mail.com>
2021-12-23 12:11:16 -0500

    findPage css

--- localeyes/frontEnd/src/components/Find.js
+++ localeyes/frontEnd/src/components/Find.js
@@ -3,10 +3,12 @@ import React from 'react';
 function Find() {
   return (
     <div className="findPage">
-      <h1>Where you can find us...</h1>
-      <h2>anywhere on those streets of cobblestone</h2>
-      <h3>You cant call us</h3>
-      <h3>You can't email us</h3>
+      <div className="findInfoBlock">
+        <h1>Where you can find us...</h1>
+        <h2>anywhere on those streets of cobblestone</h2>
+        <h3>You cant call us</h3>
+        <h3>You can't email us</h3>
+      </div>
     </div>
   );
 }
